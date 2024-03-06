7664088c7331cbe6669c6a28ddb3f23430fe4c04
wudg <321073229@qq.com>
2024-03-06 23:55:29 +0800

    feat:删除多余代码

--- ai-back/src/main/java/com/ye/aiback/controller/IndexController.java
+++ ai-back/src/main/java/com/ye/aiback/controller/IndexController.java
@@ -5,7 +5,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Api(tags = "页面跳转控制器",value = "页面跳转控制器")
 @Controller
